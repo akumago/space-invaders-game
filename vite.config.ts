34e50ea845fa28c32c0014744ecc0ad67e18c015
akumago <suteakann@gmail.com>
2025-06-02 03:23:19 +0900

    revert: Revert base path to original for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       plugins: [react()],
-      base: '/space-invaders-game/',
+      base: '/space-invaders-game/mitchie-quest/',
       define: {
         'process.env': env
       },
