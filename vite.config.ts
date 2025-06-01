337750b3d96d5f94b9529b390123efaf3f0aed3b
akumago <suteakann@gmail.com>
2025-06-02 07:07:37 +0900

    Correctly use loaded environment variable for base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    // loadEnvを使って環境変数を読み込む
+    const env = loadEnv(mode, process.cwd(), '');
     
-    // GAME_TYPE環境変数に基づいてベースパスを決定
+    // 読み込んだ環境変数GAME_TYPEに基づいてベースパスを決定
     let basePath = '/';
-    if (process.env.GAME_TYPE === 'root') {
+    if (env.GAME_TYPE === 'root') {
       basePath = '/space-invaders-game/';
-    } else if (process.env.GAME_TYPE === 'space-invaders') {
+    } else if (env.GAME_TYPE === 'space-invaders') {
       basePath = '/space-invaders-game/space-invaders/';
-    } else if (process.env.GAME_TYPE === 'mitchie-quest') {
+    } else if (env.GAME_TYPE === 'mitchie-quest') {
       basePath = '/space-invaders-game/mitchie-quest/';
     }
 
